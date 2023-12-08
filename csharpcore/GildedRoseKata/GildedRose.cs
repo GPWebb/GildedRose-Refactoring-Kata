7b242f715e42b27e5bc35e4b747cf3767f6ea4af
Greg Webb <greg.webb@alpinered.co.uk>
2023-12-08 12:53:59 +0000

    Break item update into descriptively named subroutines

--- csharpcore/GildedRoseKata/GildedRose.cs
+++ csharpcore/GildedRoseKata/GildedRose.cs
@@ -1,5 +1,4 @@
-﻿using GildedRoseKata;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace GildedRoseKata;
 
@@ -14,7 +13,7 @@ public class GildedRose
 
     public void UpdateQuality()
     {
-        foreach(var item in _items)
+        foreach (var item in _items)
         {
             UpdateItem(item);
         }
@@ -22,15 +21,18 @@ public class GildedRose
 
     private void UpdateItem(Item item)
     {
+        UpdateItemQuality(item);
+
+        if (item.Name != Constants.Sulfuras) item.SellIn--;
+
+        if (item.SellIn < Constants.SellInLow) UpdateItemBelowSellInLowQuality(item);
+    }
+
+    private static void UpdateItemQuality(Item item)
+    {
         if (item.Name != Constants.AgedBrie && item.Name != Constants.BackstagePasses)
         {
-            if (item.Quality > 0)
-            {
-                if (item.Name != Constants.Sulfuras)
-                {
-                    item.Quality--;
-                }
-            }
+            DropNonSulfurasQuality(item);
         }
         else
         {
@@ -40,55 +42,62 @@ public class GildedRose
 
                 if (item.Name == Constants.BackstagePasses)
                 {
-                    if (item.SellIn < Constants.SellInHigh)
-                    {
-                        if (item.Quality < Constants.ItemQualityUpperThreshold)
-                        {
-                            item.Quality++;
-                        }
-                    }
-
-                    if (item.SellIn < Constants.SellInMid)
-                    {
-                        if (item.Quality < Constants.ItemQualityUpperThreshold)
-                        {
-                            item.Quality++;
-                        }
-                    }
+                    UpdateBackstagePassItemQuality(item);
                 }
             }
         }
+    }
 
-        if (item.Name != Constants.Sulfuras)
+    private static void UpdateItemBelowSellInLowQuality(Item item)
+    {
+        if (item.Name != Constants.AgedBrie)
         {
-            item.SellIn--;
+            DropExpiringItemQuality(item);
+        }
+        else
+        {
+            if (item.Quality < Constants.ItemQualityUpperThreshold)
+            {
+                item.Quality++;
+            }
+        }
+    }
+
+    private static void DropExpiringItemQuality(Item item)
+    {
+        if (item.Name != Constants.BackstagePasses)
+        {
+            DropNonSulfurasQuality(item);
+        }
+        else
+        {
+            item.Quality = 0;
+        }
+    }
+
+    private static void DropNonSulfurasQuality(Item item)
+    {
+        if (item.Quality > 0 && item.Name != Constants.Sulfuras)
+        {
+            item.Quality--;
+        }
+    }
+
+    private static void UpdateBackstagePassItemQuality(Item item)
+    {
+        if (item.SellIn < Constants.SellInHigh)
+        {
+            if (item.Quality < Constants.ItemQualityUpperThreshold)
+            {
+                item.Quality++;
+            }
         }
 
-        if (item.SellIn < Constants.SellInLow)
+        if (item.SellIn < Constants.SellInMid)
         {
-            if (item.Name != Constants.AgedBrie)
+            if (item.Quality < Constants.ItemQualityUpperThreshold)
             {
-                if (item.Name != Constants.BackstagePasses)
-                {
-                    if (item.Quality > 0)
-                    {
-                        if (item.Name != Constants.Sulfuras)
-                        {
-                            item.Quality--;
-                        }
-                    }
-                }
-                else
-                {
-                    item.Quality = 0;
-                }
-            }
-            else
-            {
-                if (item.Quality < Constants.ItemQualityUpperThreshold)
-                {
-                    item.Quality++;
-                }
+                item.Quality++;
             }
         }
     }
